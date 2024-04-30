1bd4f4fabd7bf0a6fba40d180bf6cf7ee75545c7
SherlacAlex <sherlacalexander@gmail.com>
2024-04-30 23:29:47 +0530

    auth store for user details

---
+++ src/store/AuthSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { UserAuth } from "../models/UserAuth";
+
+const initialState: UserAuth = new UserAuth({
+    loggedStatus: false,
+    userData: null
+})
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState, 
+    reducers: {
+        logIn: (state, action) => {
+            state.loggedStatus = true;
+            state.userData = action.payload.userData;
+        },
+        logOut: (state) => {
+            state.loggedStatus = false;
+            state.userData = null;
+        }
+    }
+});
+
+export default authSlice.reducer;
